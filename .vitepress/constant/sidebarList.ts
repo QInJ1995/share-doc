9d4977beeed702246162bd13301e0a51d069d2f9
QINJIN <953373752@qq.com>
2024-11-25 15:04:11 +0800

    doc: 更新文档

--- .vitepress/constant/sidebarList.ts
+++ .vitepress/constant/sidebarList.ts
@@ -223,7 +223,64 @@ export const sidebar = {
                 {
                     text: '进阶',
                     collapsed: true,
-                    items: []
+                    items: [
+                        {
+                            text: '面向对象进阶(一)',
+                            link: 'docs/java/JavaSE/进阶/面向对象进阶(一)'
+                        },
+                        {
+                            text: '面向对象进阶(二)',
+                            link: 'docs/java/JavaSE/进阶/面向对象进阶(二)'
+                        },
+                        {
+                            text: '面向对象进阶(三)、常见API',
+                            link: 'docs/java/JavaSE/进阶/面向对象进阶(三)、常见API'
+                        },
+                        {
+                            text: '常见API',
+                            link: 'docs/java/JavaSE/进阶/常见API'
+                        },
+                        {
+                            text: '常见API、Lambda、算法、正则',
+                            link: 'docs/java/JavaSE/进阶/常见API、Lambda、算法、正则'
+                        },
+                        {
+                            text: '异常、集合、List集合',
+                            link: 'docs/java/JavaSE/进阶/异常、集合、List集合'
+                        },
+                        {
+                            text: 'Set集合、Map集合',
+                            link: 'docs/java/JavaSE/进阶/Set集合、Map集合'
+                        },
+                        {
+                            text: 'Map集合、Stream、File',
+                            link: 'docs/java/JavaSE/进阶/Map集合、Stream、File'
+                        },
+                        {
+                            text: '字符集、IO流(一)',
+                            link: 'docs/java/JavaSE/进阶/字符集、IO流(一)'
+                        },
+                        {
+                            text: 'IO流(二)',
+                            link: 'docs/java/JavaSE/进阶/IO流(二)'
+                        },
+                        {
+                            text: '特殊文件、日志技术、多线程',
+                            link: 'docs/java/JavaSE/进阶/特殊文件、日志技术、多线程'
+                        },
+                        {
+                            text: '多线程',
+                            link: 'docs/java/JavaSE/进阶/多线程'
+                        },
+                        {
+                            text: '网络编程',
+                            link: 'docs/java/JavaSE/进阶/网络编程'
+                        },
+                        {
+                            text: '单元测试、反射、注解、动态代理',
+                            link: 'docs/java/JavaSE/进阶/单元测试、反射、注解、动态代理'
+                        },
+                    ]
                 },
             ]
         },
