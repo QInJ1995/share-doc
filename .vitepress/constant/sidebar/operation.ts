ae65a4170a9b712d71e16cd78cfd9e8d3a04d4fc
QINJIN <953373752@qq.com>
2025-03-27 00:47:37 +0800

    doc: 更新文档

---
+++ .vitepress/constant/sidebar/operation.ts
@@ -0,0 +1,34 @@
+export default [
+    {
+        text: '文档',
+        link: 'docs/operation/index'
+    },
+    {
+        text: 'Linux',
+        collapsed: false,
+        items: [
+            {
+                text: '基础教程',
+                link: 'docs/operation/Linux/基础教程.md'
+            },
+            {
+                text: '进阶教程',
+                link: 'docs/operation/Linux/进阶教程.md'
+            }
+        ],
+    },
+    {
+        text: 'Docker',
+        collapsed: false,
+        items: [
+            {
+                text: '基础教程',
+                link: 'docs/operation/docker/基础教程.md'
+            },
+            {
+                text: '进阶教程',
+                link: 'docs/operation/docker/进阶教程.md'
+            }
+        ],
+    },
+]
